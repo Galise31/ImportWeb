9c82226129579bac48a7c7fe3ccc2d0fa9439ee2
SEVESTRE <sav@importweb.fr>
2018-10-23 18:55:20 +0200

    Gestion de LogResultat dans le proxy

--- ImportWeb/IWMain.cs
+++ ImportWeb/IWMain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace ImportWeb
@@ -15,7 +16,9 @@ namespace ImportWeb
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-        
+            // Pour trouver le proxy ailleurs
+            AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolveHandler;
+
             try
             {
                 IwTools.loIW = new ImportWeb_Proxy.Proxy();
@@ -29,6 +32,42 @@ namespace ImportWeb
                 return;
 
             Application.Run(new Start());
+        }
+
+        internal static Assembly AssemblyResolveHandler(object sender, ResolveEventArgs args)
+        {
+            // This handler is called only when the common language runtime tries to bind to the assembly and fails.
+
+            //Retrieve the list of referenced assemblies in an array of AssemblyName.
+            Assembly MyAssembly, objExecutingAssemblies;
+            string strTempAssmbPath = "";
+
+            objExecutingAssemblies = Assembly.GetExecutingAssembly();
+            AssemblyName[] arrReferencedAssmbNames = objExecutingAssemblies.GetReferencedAssemblies();
+
+            //Loop through the array of referenced assembly names.
+            foreach (AssemblyName strAssmbName in arrReferencedAssmbNames)
+            {
+                //Check for the assembly names that have raised the "AssemblyResolve" event.
+                if (strAssmbName.FullName.Substring(0, strAssmbName.FullName.IndexOf(",")) == args.Name.Substring(0, args.Name.IndexOf(",")))
+                {
+                    IniFile loIni = new IniFile(AppDomain.CurrentDomain.BaseDirectory + "IW_Login.txt");
+                    strTempAssmbPath = loIni.Read("ImportWeb", "ProxyPath");
+
+                    if (strTempAssmbPath == string.Empty)
+                        strTempAssmbPath = @"C:\Program Files (x86)\Galise\ImportWeb\";
+
+                    if (strTempAssmbPath.EndsWith("\\")) strTempAssmbPath += "\\";
+
+                    strTempAssmbPath += args.Name.Substring(0, args.Name.IndexOf(",")) + ".dll";
+                    break;
+                }
+            }
+            //Load the assembly from the specified path.
+            MyAssembly = Assembly.LoadFrom(strTempAssmbPath);
+
+            //Return the loaded assembly.
+            return MyAssembly;
         }
     }
 }
